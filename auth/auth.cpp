53bdb989c275019c0ac3ba1f01ebf98068d2db46
sarkis234 <sarkis95283@gmail.com>
2025-02-23 14:07:05 +0300

    Update and rename TestSystem.cpp to auth.cpp

---
+++ auth/auth.cpp
@@ -0,0 +1,37 @@
+#include <iostream>﻿
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    while (true) {
+        string login;
+        string password;
+        ifstream in("users.txt");
+        string line;
+        bool loginFlag = false;
+        bool passwordFlag = false;
+        cout << "Enter your login: ";
+        cin >> login;
+        cout << "Enter your password: ";
+        cin >> password;
+        while (getline(in, line, ':')) {
+            if (login.compare(line) == 0) {
+                loginFlag = true;
+            }
+            getline(in, line);
+            if (password.compare(line) == 0) {
+                passwordFlag = true;
+            }
+        }
+        if (loginFlag == false || passwordFlag == false) {
+            cout << "incorrect login or password!" << endl;
+            continue;
+        }
+        else {
+            cout << "You entered system succesfully!" << endl;
+            break;
+        }
+    }
+}
