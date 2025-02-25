9bef400eee67048ae250b80d33ded73ab4e24fb0
mila0408 <159796730+mila0408@users.noreply.github.com>
2025-02-25 22:31:38 +0300

    Update User.cpp

--- TestingMain/User.cpp
+++ TestingMain/User.cpp
@@ -2,5 +2,25 @@
 
 void User::registration()
 {
-
+  string login; string buf;
+  string password;
+  cout << "enter your login and then the password on the new line:" << endl;
+  cin >> login;
+  cin >> password;
+  
+  fstream file("login_And_Pasword.txt");
+  while (getline(file, buf, ':')) {
+  	if (buf == login) {
+  		cout << "the login is already occupied, choose another one: " << endl;
+  		cin >> login;
+  		registration();
+  	}
+  	else{
+  		// login = функция шифрования 
+  		// password = функция шифрования 
+  		file << login << ':' << password;}
+  	
+  }
+  
+  file.close();
 }
